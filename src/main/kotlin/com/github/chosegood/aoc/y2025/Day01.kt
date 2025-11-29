f5f4d1ff92a72ed9d813ecc8fa8e213348acced2
Chris Hosegood <chris.hosegood@gmail.com>
2025-11-29 11:21:52 -0500

    y2025 skeleton

---
+++ src/main/kotlin/com/github/chosegood/aoc/y2025/Day01.kt
@@ -0,0 +1,20 @@
+package com.github.chosegood.aoc.y2025
+
+import com.github.chosegood.aoc.AdventOfCode
+import com.github.chosegood.aoc.InputReader
+
+class Day01 : AdventOfCode {
+
+    override fun part1(input: List<String>): Long {
+        return 0L
+    }
+
+    override fun part2(input: List<String>): Long {
+        return 0L
+    }
+}
+
+fun main() {
+    println(Day01().part1(InputReader.readInput("D0101_input.txt")))
+    println(Day01().part2(InputReader.readInput("D0102_input.txt")))
+}
---
+++ src/test/kotlin/com/github/chosegood/aoc/y2025/Day01Test.kt.kt
@@ -0,0 +1,14 @@
+package com.github.chosegood.aoc.y2025
+
+import org.junit.jupiter.api.Test
+
+class `Day01Test.kt` {
+
+    @Test
+    fun part1() {
+    }
+
+    @Test
+    fun part2() {
+    }
+}
