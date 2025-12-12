c25edeccdd94b4d66aa189cd804e327b49fcb272
Chris Hosegood <chris.hosegood@gmail.com>
2025-12-12 13:33:24 -0500

    y2025 Empty day07

---
+++ src/main/kotlin/com/github/chosegood/aoc/y2025/Day07.kt
@@ -0,0 +1,22 @@
+package com.github.chosegood.aoc.y2025
+
+import com.github.chosegood.aoc.AdventOfCode
+import com.github.chosegood.aoc.InputReader
+
+fun main() {
+    println("Part1: " + Day07().part1(InputReader.readInput("D0701_input.txt")))
+    println("Part2: " + Day07().part2(InputReader.readInput("D0701_input.txt")))
+}
+
+class Day07 : AdventOfCode {
+
+
+    override fun part1(input: List<String>): Long {
+        return 0L
+    }
+
+    override fun part2(input: List<String>): Long {
+        return 0L
+    }
+
+}
